0d633ae654955b7fafbd2dad67b43d01e57b94ac
HAREIN05 <145885600+HAREIN05@users.noreply.github.com>
2026-06-21 12:41:42 +0800

    Update heap_sort_step.cpp

--- src/heap_sort_step.cpp
+++ src/heap_sort_step.cpp
@@ -6,13 +6,13 @@
 // Trimester: 2610
 // Member_1: 242UC243BE | EBA MOHAMED ABBAS AHMED    | +60136734355
 // Member_2: 242UC244SN | MOHAMMAD IEMAN BIN ZAHARI  | +60183756140
-// Member_3: 242UC243B4 | LAMA M. R. SIAM            | +60183942734
-// Member_4: 242UC244L3 | HAREIN A/L SATHIAMURTHY    | +60143653986
+// Member_3: 242UC244L3 | HAREIN A/L SATHIAMURTHY    | +60143653986
+// Member_4: 242UC243B4 | LAMA M. R. SIAM            | +60183942734
 // *********************************************************
 // Task Distribution
-// Member_1: Radix Sort
+// Member_1: Dataset Generator + Radix Sort
 // Member_2: Radix Sort Step + Heap Sort
-// Member_3: Heap Sort Step + Hash Table Search
+// Member_3: Hash Table Search + Heap Sort Step
 // Member_4: Hash Table Search Step + Conclusion
 // *********************************************************
 
